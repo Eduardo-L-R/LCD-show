11b779d8216952cc052a32deb2365e24320e1f91
Eduardo-L-R <60417224+Eduardo-L-R@users.noreply.github.com>
2020-11-14 19:18:20 -0300

    Update and rename desabilitar-suspencion to desabilitar-suspencion.sh

---
+++ desabilitar-suspencion.sh
@@ -0,0 +1,9 @@
+export DISPLAY=:0 &&
+xset q &&#Muestra estado de xset
+xset s off &&#No activa suspension
+xset -dpms &&#Deshabilita las caracteristicas DPMS (ahorro de energia)
+xset s noblank &&#No activa el salva pantallas blanco
+#xset s on &&#activa suspension
+#xset +dpms &&#habilita las caracteristicas DPMS (ahorro de energia)
+#xset s blank &&#activa el salva pantallas blanco
+xset q #Muestra estado de xset modificado
